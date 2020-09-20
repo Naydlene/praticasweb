947c5b2a8cdf39b7ad05e9442e69c9b06c3d46f9
Naydlene Vitoria <naydlenevitoria@gmail.com>
2020-09-20 11:13:43 -0300

    Primeiro commit

---
+++ web-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ContadorComponent } from './contador/contador.component';
+import { ComponentComponent } from './card-perfil/component/card-perfil.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RodapeComponent,
+    ContadorComponent,
+    ComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ web-app/src/app/card-perfil/component/card-perfil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './card-perfil.component.html',
+  styleUrls: ['./card-perfil.component.css']
+})
+export class ComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
